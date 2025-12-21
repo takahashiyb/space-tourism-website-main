0f2c40b3c8ead322218c98704bcc30f905e6870d
takahashiyb <takahashiyb89@gmail.com>
2025-12-22 00:15:50 +0800

    Add dialog tag to open navigation when screen size is lower than 700px.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 import { pagination } from "./pagination.js";
 import { loadDestinationTabs } from "./destinations-tab.js";
+import { functionHamburger } from "./navigation.js";
 
 loadDestinationTabs();
 pagination("components-small-pagination", "crew");
 pagination("components-large-pagination", "technology");
+functionHamburger();
---
+++ js/navigation.js
@@ -0,0 +1,21 @@
+export function functionHamburger() {
+  const dialog = document.querySelector(".js-dialog");
+
+  const buttonClose = document.querySelector(".js-button_close");
+
+  const buttonHamburger = document.querySelector(".header__menu");
+
+  buttonHamburger.addEventListener("click", () => {
+    dialog.showModal();
+  });
+
+  buttonClose.addEventListener("click", () => {
+    dialog.close();
+  });
+
+  const media = window.matchMedia("(min-width: 43.75em)");
+
+  media.addEventListener("change", (e) => {
+    dialog.close();
+  });
+}
